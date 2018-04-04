b0ea49f018e45e727fd335973e206b74b081f61b
mohankumargupta <mohangupta@live.com>
2018-04-04 16:52:04 +1000

    written my first unit test ever

---
+++ test/unit/specs/routing.spec.js
@@ -0,0 +1,24 @@
+import DockerHelper from '@/components/MainPage/DockerHelper'
+
+test('replenish docker containers', () => {
+  const config = {
+    'DOCKER': {
+        'web': true,
+        'grafana': false
+    }
+  }
+  let dockerContainers = []
+  var expected = [
+    {
+      text: 'web',
+      value: true
+    },
+    {
+      text: 'grafana',
+      value: false
+    }
+  ]
+  DockerHelper.mounted(config, dockerContainers)
+  expect(dockerContainers).toEqual(expected)
+})
+
